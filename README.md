0eaee3302cefc3f8b0cb1177e594acdb320d6338
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-03-31 03:45:46 -0500

    completed server without db

---
+++ README.md
@@ -0,0 +1,6 @@
+# Commands
+-To run make sure you have go installed and then head to a terminal
+-Type 
+```
+go run main.go bird_handlers.go
+```
--- main.go
+++ main.go
@@ -3,14 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+/*
+new router func creates the router and
+returns it to us
+now we can use this function to  create instances of the router and test the router outside of the main function
+
+
+*/
+func newRouter() *mux.Router {
+	//Router init.
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	//This is the ssr handlers.
+	staticFileDir := http.Dir("./assets/")
+	staticFileHandler := http.StripPrefix("/assets", http.FileServer(staticFileDir))
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	//Handle bird code.
+	r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
+	return r
+}
+
+func main() {
+	// The router is now formed by calling the `newRouter` constructor function
+	// that we defined above. The rest of the code stays the same
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Sup?")
-}
-func main() {
-	PORT := ":5000"
-	http.HandleFunc("/", handler)
-	fmt.Println("//=====Listening on port", PORT)
-	http.ListenAndServe(PORT, nil)
 }
